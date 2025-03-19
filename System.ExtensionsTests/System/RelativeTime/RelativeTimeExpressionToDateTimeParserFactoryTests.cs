dfb0e2aaed88dab90b8da6c38d03f99dca5604c4
ChaeSeongSik <css5831@outlook.com>
2025-03-19 13:39:17 +0900

    RelativeTimeToDateTime Parser  Modified #2

--- System.ExtensionsTests/System/RelativeTime/RelativeTimeExpressionToDateTimeParserFactoryTests.cs
+++ System.ExtensionsTests/System/RelativeTime/RelativeTimeExpressionToDateTimeParserFactoryTests.cs
@@ -10,75 +10,75 @@ namespace System.RelativeTime.Tests
 		{
 			IRelativeTimeToDateTimeParser parser = RelativeTimeExpressionToDateTimeParserFactory.Create();
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.NowPart());
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.NowPart(), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.ModifierPart(Ast.IRelativeTimeExpression.OperatorType.PLUS, TimeGranularityUnit.SECONDS, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.ModifierPart(Ast.IRelativeTimeExpression.OperatorType.PLUS, TimeGranularityUnit.SECONDS, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.ModifierPart(Ast.IRelativeTimeExpression.OperatorType.PLUS, TimeGranularityUnit.SECONDS));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.ModifierPart(Ast.IRelativeTimeExpression.OperatorType.PLUS, TimeGranularityUnit.SECONDS), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.ModifierPart(Ast.IRelativeTimeExpression.OperatorType.MINUS, TimeGranularityUnit.SECONDS));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.ModifierPart(Ast.IRelativeTimeExpression.OperatorType.MINUS, TimeGranularityUnit.SECONDS), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.SECONDS, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.SECONDS, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.MINUTES, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.MINUTES, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.HOURS, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.HOURS, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.DAYS, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.DAYS, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK1, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK1, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK2, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK2, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK3, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK3, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK4, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK4, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK5, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK5, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK6, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.WEEK6, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.MONTHS, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.MONTHS, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.QUARTER, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.QUARTER, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
-				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.YEARS, null));
+				DateTime dateTime = parser.Parse(new Ast.IRelativeTimeExpression.SnapPart(Ast.IRelativeTimeExpression.SnapTimeUnit.YEARS, null), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 			{
@@ -91,7 +91,7 @@ namespace System.RelativeTime.Tests
 						Ast.IRelativeTimeExpression.SnapTimeUnit.DAYS, 
 						new Ast.IRelativeTimeExpression.ModifierPart(
 							Ast.IRelativeTimeExpression.OperatorType.PLUS, 
-							TimeGranularityUnit.MINUTES, 5))));
+							TimeGranularityUnit.MINUTES, 5))), DateTime.Now);
 				Trace.WriteLine($"{dateTime:yyyy-MM-ddTHH:mm:ss.fff}");
 			}
 		}
